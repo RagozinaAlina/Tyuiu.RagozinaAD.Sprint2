e565966830fc0dd56c8ae11f7abe4b91d80e0ebf
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-09-29 17:46:11 +0500

    Add project files.

---
+++ Tyuiu.RagozinaAD.Sprint2.Task0.V17/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task0.V17.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        int x = 1065;
+        int y = 755;
+        bool[] res = new bool[6];
+        res = ds.GetCompareOperations(x, y);
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #17                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу из операций сравнений и арифметических выражений,    *");
+        Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("X = " + x);
+        Console.WriteLine("Y = " + y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(res[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
