4f31bd95903f61cc0d9a144c96ac869af8423ec2
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-01 15:32:19 +0500

    Task2

---
+++ Tyuiu.RagozinaAD.Sprint2.Task2.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task2.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения двух    *");
+        Console.WriteLine("* одинаковых масссивов по длинне.                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение переменной X:");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите значение переменной Y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в заштрихованной области");
+        }
+
+        Console.ReadKey();
+
+    }
+}
