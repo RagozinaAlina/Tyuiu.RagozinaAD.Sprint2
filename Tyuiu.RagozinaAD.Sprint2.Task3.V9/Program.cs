cce9055d17da6f8867f90681dd394a08079b6174
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-02 19:16:23 +0500

    Task3

---
+++ Tyuiu.RagozinaAD.Sprint2.Task3.V9/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task3.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с    *");
+        Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит      *");
+        Console.WriteLine("* значение переменной X с клавиатуры.                                     *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение X:");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double res = ds.Calculate(x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Значение функции = " + res);
+
+        Console.ReadKey();
+    }
+}
