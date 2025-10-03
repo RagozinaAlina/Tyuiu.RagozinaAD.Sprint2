8d84e7497c7bda9bf87366bf203492555b6c0eee
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-03 17:15:38 +0500

    Task4

--- Tyuiu.RagozinaAD.Sprint2.Task4.V26.Lib/DataService.cs
+++ Tyuiu.RagozinaAD.Sprint2.Task4.V26.Lib/DataService.cs
@@ -11,21 +11,3 @@ namespace Tyuiu.RagozinaAD.Sprint2.Task4.V26.Lib
         }
     }
 }
-public void ValidCondition1()
-{
-    DataService ds = new DataService();
-    double x = 1;
-    double y = 2;
-    double res = ds.Calculate(x, y);
-    double wait = 144;
-    Assert.AreEqual(wait, res);
-}
-public void ValidCondition2()
-{
-    DataService ds = new DataService();
-    double x = 1;
-    double y = 2;
-    double res = ds.Calculate(x, y);
-    double wait = 0.5;
-    Assert.AreEqual(wait, res);
-}
