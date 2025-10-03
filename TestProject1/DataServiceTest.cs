724148872e6701624708fd3eadbdaca25f5f9fcb
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-03 16:38:03 +0500

    Task4

---
+++ TestProject1/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿
+using Tyuiu.RagozinaAD.Sprint2.Task4.V26.Lib;
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 144;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 0.5;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
