2ee7690595ad00f661b49add88392bb17950bd73
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-03 18:31:04 +0500

    Task5

---
+++ Tyuiu.RagozinaAD.Sprint2.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task5.V11.Lib;
+namespace Tyuiu.RagozinaAD.Sprint2.Task5.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+
+            int g = 2025;
+            int m = 10;
+            int n = 3;
+            var res = ds.FindDateOfNextDay(g, m, n);
+            string wait = "04.10.2025";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint2.Task5.V11/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task5.V11.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует оператор switch                  *");
+        Console.WriteLine("* вычисляет требуемое значение и возвращает результат.                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение year: ");
+        int g = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение month: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение day: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.FindDateOfNextDay(g, m, n));
+
+        Console.ReadKey();
+    }
+}
