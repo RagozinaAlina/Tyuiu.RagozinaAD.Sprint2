8b6ce9db2887056c3a84974da9759f4f59c6d47b
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-06 12:32:36 +0500

    Task7

---
+++ Tyuiu.RagozinaAD.Sprint2.Task7.V3.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2; 
+namespace Tyuiu.RagozinaAD.Sprint2.Task7.V3.Lib
+{
+    public class DataService : ISprint2Task7V3
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool res;
+            if ((Math.Pow(x, 2) + Math.Pow(y-1, 2) <= 1) && (x > 0 && (x <= 1) && ((y > 0)) && (x <= 1)))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint2.Task7.V3/Program.cs
@@ -0,0 +1,51 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task7.V3.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #3                                                              *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать консольную программу, запрашивает исходные данные и вычисляет, *");
+        Console.WriteLine("* находится ли точка с координатами X,Y в заштрихованной области.         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите значение переменной x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение переменной y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        bool res = ds.CheckDotInShadedArea(x, y);
+
+        if (res)
+        {
+            Console.WriteLine("Точка находится в заштрихованной области");
+        }
+        else
+        {
+            Console.WriteLine("Точка не находится в заштрихованной области");
+        }
+
+        Console.ReadKey();
+    }
+}
+
+
