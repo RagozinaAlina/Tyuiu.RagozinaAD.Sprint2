46c5b7a316257a0b8fbd18981b12206059ae2289
Алина Рагозина <Tyuiu.RagozinaAD@outlook.com>
2025-10-06 11:46:30 +0500

    Task6

---
+++ Tyuiu.RagozinaAD.Sprint2.Task6.V9.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task6.V9.Lib;
+namespace Tyuiu.RagozinaAD.Sprint2.Task6.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfNextDay()
+        {
+            DataService ds = new DataService();
+
+            int n = 06;
+            int m = 28;
+            string wait = "29.06";
+            var res = ds.FindDateOfNextDay(m, n);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.RagozinaAD.Sprint2.Task6.V9/Program.cs
@@ -0,0 +1,69 @@
+﻿using Tyuiu.RagozinaAD.Sprint2.Task6.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #2 | Выполнила: Рагозина А.Д | АСОиУБ-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #9                                                              *");
+        Console.WriteLine("* Выполнила: Рагозина А.Д | АСОиУБ-25-1                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Условие:                                                                *");
+        Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+        Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результа     *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДВННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+
+        Console.WriteLine("Введите порядковый номер месяца: ");
+
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        if ((n > 12) || (m > 31))
+        {
+            Console.WriteLine("Введены неправильные данные");
+        }
+        else
+        {
+            if ((n == 2) && (m > 28))
+            {
+                Console.WriteLine("Такого дня не существует");
+            }
+            else if ((n == 4) && (m > 30))
+            {
+                Console.WriteLine("Такого дня не существует");
+            }
+            else if ((n == 6) && (m > 30))
+            {
+                Console.WriteLine("Такого дня не существует");
+            }
+            else if ((n == 9) && (m > 30))
+            {
+                Console.WriteLine("Такого дня не существует");
+            }
+            else if ((n == 11) && (m > 30))
+            {
+                Console.WriteLine("Такого дня не существует");
+            }
+            else
+            {
+                Console.WriteLine("********************************************************************************************");
+                Console.WriteLine("* РЕЗУЛЬТАТ:                                                                               *");
+                Console.WriteLine("********************************************************************************************");
+                Console.WriteLine("Следующий день будет " + ds.FindDateOfNextDay(m, n));
+
+            }
+        }
+        Console.ReadKey();
+    }
+}
